6c7ae067ad31f46041344b45218b82ec02026444
subinacls <admin@hacker-island.com>
2018-06-10 03:43:02 -0600

    Update twitter.py
    modified for more standard conformation
    
    still needs work 
    
    no dump to file yet

--- plugins/twitter.py
+++ plugins/twitter.py
@@ -1,3 +1,6 @@
+#
+# Twitter Scraper: Requires users to install additional non standard libraries
+#
 from plugins.base import PageGrabber
 from colors import BodyColors as bc
 try:
@@ -5,101 +8,113 @@ try:
 except:
     import builtins as bi
 import time
-from bs4 import BeautifulSoup as bs
-from selenium.webdriver import Firefox
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.options import Options
-
+try:
+ from bs4 import BeautifulSoup as bs
+except Exception as e:
+ print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Failed at importing BeautifulSoup from bs4: {}\n"+bc.CEND).format(e)
+try:
+ from selenium.webdriver import Firefox
+ from selenium.webdriver.common.by import By
+ from selenium.webdriver.common.keys import Keys
+ from selenium.webdriver.support.ui import Select
+ from selenium.webdriver.support.ui import WebDriverWait
+ from selenium.webdriver.firefox.options import Options
+except Exception as e:
+ print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Failed at importing selenium requirements: {}\n"+bc.CEND).format(e)
+try:
+ from tqdm import tqdm
+except Exception as e:
+ print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Failed at importing tqdm from tdqm: {}\n"+bc.CEND).format(e)
+import os
 
 class TwitterGrabber(PageGrabber):
  def get_info(self, screenname):
-  print screenname
   print("["+bc.CPRP+"?"+bc.CEND+"] "+bc.CCYN + "Twitter" + bc.CEND)
+  print(" ["+bc.CGRN+"!"+bc.CEND+"] "+bc.CRED+"Module takes some time to load, please wait!"+bc.CEND)
   options = Options()
-  print "Set options"
   try:
    options.add_argument('--headless')
    b=Firefox(executable_path='/usr/bin/geckodriver',firefox_options=options)
    wait = WebDriverWait(b, timeout=5)
    b.get('https://twitter.com/{}'.format(screenname))
   except Exception as e:
-   print e
-  print "Sent request"
+   print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Failed at making the initial request: {}\n"+bc.CEND).format(e)
   try:
-   h = b.page_source
-   soup = bs(h,'lxml')
+   soup = bs(b.page_source,'lxml')
   except Exception as e:
-   print e
-  print "got soup"
+   print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Unable to make initial soup: {}\n"+bc.CEND).format(e)
   validname = str(soup.h1).split()[-2].split('/')[1].split('"')[0]
-  avatar=str(soup.findAll('img', {'class','avatar','js-action-profile-avatar'})[3]['src'])
+  avatar = str(soup.findAll('img', {'class','avatar','js-action-profile-avatar'})[3]['src'])
   profnav = soup.find_all('span',{'class','ProfileNav-value'})
   try:
    if len(profnav) >= 5:
     datal = list()
-    print "making list"
     for x in profnav[0:len(profnav)-1:]:
      datal.append(" ".join(str(x).replace("\n"," ").split()).split('"')[3])
-  except:
-   print "Failed to get profnav"
-  print datal
+  except Exception as e:
+   print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Failed at making the datalist: {}\n"+bc.CEND).format(e)
   tcount,fgcount, fscount, likes = datal[:4]
   page = int(tcount) / 20 + 1
-  print tcount
-  print page
-  print fgcount
-  print fscount
-  print likes
+  nap = 1
+  estt = page*nap/60
   try:
-   for i in range(1,page):
-    b.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(1)
+   scrapeall = raw_input(" [!] Do you want to capture all tweets ?\n [!] Estimated time to complete: "+str(estt)+"m (Y/n) ")
+   if scrapeall.lower() in [1,'y','true','on']:
+    for i in tqdm(range(1,page)):
+     b.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+     time.sleep(nap)
   except Exception as e:
-   print e
+   print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Failed at scrolling site: {}\n"+bc.CEND).format(e)
   try:
    h = b.page_source
-   soup = bs(h,'html')
+   soup = bs(h,'lxml')
   except Exception as e:
-   print e
-  print "Get 2nd helping"
+   print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Can not make soup, phase 2: {}\n"+bc.CEND).format(e)
   datelist = list()
   timelist = list()
   for d in soup.findAll('li', {'class','js-stream-item'}):
-   print "  [+]", "-" *80
+   print "\n  [+]", "-" *80
    if 'Retweeted' in d.p.text:
     print(" ["+bc.CGRN+"!"+bc.CEND+"] "+bc.CRED+"Retweet: "+bc.CEND)
-    pass
    if str(d.span).split()[3] == 'Icon--pinned':
     print(" ["+bc.CGRN+"!"+bc.CEND+"] "+bc.CRED+"Pinned: "+bc.CEND)
-    pass
    try:
     tlist = d.findAll('a')
     dt = tlist[1]['title']
    except Exception as e:
-    #print "tlist Fail: %s" % e
-    #print tlist
-    dt = tlist[2]['title']
-    pass
+    try:
+     dt = tlist[2]['title']
+    except Exception as e:
+     print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Unable to find datetime: {}\n"+bc.CEND).format(e)
+     pass
    datelist.append(dt)
    print("  ["+bc.CGRN+"+"+bc.CEND+"] "+bc.CRED+"Date/Time: "+bc.CEND+ str(dt))
    try:
     timestamp = str(tlist[1]).split()[-3].split('"')[1]
+    if len(timestamp) < 10:
+     timestamp = str(tlist[2]).split()[-3].split('"')[1]
+    if len(timestamp) < 10:
+     timestamp = "Conversation Extension"
    except Exception as e:
-    timestamp = str(tlist[2]).split()[-3].split('"')[1]
+    print e
     pass
-   timelist.append(timestamp)
-   print("  ["+bc.CGRN+"+"+bc.CEND+"] "+bc.CRED+"Timestamp: "+bc.CEND+str(timestamp))
+   if timestamp:
+    print("  ["+bc.CGRN+"+"+bc.CEND+"] "+bc.CRED+"Timestamp: "+bc.CEND+str(timestamp))
    try:
     posttitle = str(d.p.a['title'])
     print("  ["+bc.CGRN+"+"+bc.CEND+"] "+bc.CRED+"Title: "+bc.CEND+str(posttitle))
    except Exception as e:
+    #print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Unable to find title: {}\n"+bc.CEND).format(e)
     pass
    try:
     postdata = d.p.text
-    print("  ["+bc.CGRN+"+"+bc.CEND+"] "+bc.CRED+"Content: "+bc.CEND + postdata)
+    print("  ["+bc.CGRN+"+"+bc.CEND+"] "+bc.CRED+"Content:\n"+bc.CEND)
+    print postdata
    except Exception as e:
-    print "Post data failed: %s" % e
+    print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Unable to find content: {}\n"+bc.CEND).format(e)
     pass
+  try:
+   for xproc in os.popen('ps -A xf | grep -v grep | grep "/usr/bin/firefox -marionette --headless -profile /" | cut -d " " -f2'):
+    os.popen('kill -9 '+str(xproc))
+  except Exception as e:
+   print ("  ["+bc.CRED+"X"+bc.CEND+"] "+bc.CYLW+"Unable to kill Firefox headless: {}\n"+bc.CEND).format(e)
