0fb8ee081d60e7fd0a8eccd90b803dfa8467e0d7
subinacls <william@insider.agency>
2018-08-12 15:40:17 -0600

    Modified the help meu for additiona information - no format

--- plugins/menus.py
+++ plugins/menus.py
@@ -34,8 +34,57 @@ except:
 
 class menus():
 
-  def help(self):
-    print("Describe application here")
+  def helpmenu(self):
+    try:
+     os.system('clear')
+     Logo().banner()
+     print("\t[INFORMATION]::")
+     print("""
+
+This application is designed to query and parse 3rd party services in an automated fashion,
+to increase productivity, while conducting a background investigation. This application
+can be useful when trying to find hard to allocate targets. The following represent the types
+of searches that can be performed.
+
+\tEmail: 'Investigate with known email address'
+\tName: 'Investigate with knwon First/Last name'
+\tPhone: 'Investigate with known Phone Number'
+\tScreenName: 'Investigate with known Screen Name'
+\tPlate: 'Investigate with known License Plate'
+
+Each of these catagories offers different modules that request 3rd party sites after the information
+has been submitted by the user. for example the application may request a target email address.
+
+Using these classifiers, can reveal additional information that can be utiized within the application.
+These classifiers may reveal telephone, physicall address, or other useful data.
+
+All modules included in the classifier may be run with the 'ALL' qualifier or individually. Additionally,
+users can choose to reset the query string and continue using the same interface without having to restart
+the application.
+
+The following section will detail specifics about the modules offered for each classifier.
+
+:: EMAIL ::
+  Requires a user to supply a fully qualified Email address:
+
+  -: Format: username@domain.tld
+
+  This class of searches includes the following modules:
+
+  -:  LinkedIn - Check if user exposes information through LinkedIn
+  -:  HaveIBeenPwned - Check email against known compromised networks
+  -:  Myspace - Check if users account has a registered account
+  -:  WhoisMind - Check email to registered domains
+  -:  AdvancedBackgroundChecks - Run email through public page of paid access
+
+
+
+
+""")
+     not raw_input("\nPress 'ENTER' key now to continue")
+     self.intromenu()
+    except Exception as helpfail:
+     print(("Help failed: %s") & helpfail)
 
   def intromenu(self):
     bi.search_string = None
