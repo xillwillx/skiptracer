167f357e309c23f41a733cb112afcdbb9346b9bd
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2020-06-30 09:03:41 -0400

    Added missing file back to GitHub

---
+++ plugins/colors.py
@@ -0,0 +1,11 @@
+class BodyColors:  # Sets colorization for application use
+    def __init__(self):  # Initialize the values for color
+        pass
+    CCYN = '\033[96m'
+    CRED = '\033[91m'
+    CGRN = '\033[92m'
+    CYLW = '\033[93m'
+    CBLU = '\033[94m'
+    CPRP = '\033[95m'
+    CEND = '\033[0m'
+    CFON = '\33[5m'
